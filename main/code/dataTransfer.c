6141ab2b5c95387125fd6c1adf9236067e75cda1
Yes0kk <92611294+Yes0kk@users.noreply.github.com>
2026-06-13 15:45:59 -0400

    Deleted blank datatransfer files

--- main/code/dataTransfer.c
+++
@@ -1,1 +0,0 @@
-#include "dataTransfer.h"
--- main/code/dataTransfer.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "c1101/c1101.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_adc/adc_oneshot.h"
-#include "driver/gpio.h"
-
-
